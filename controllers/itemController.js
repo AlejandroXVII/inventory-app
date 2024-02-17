626feb254379cb5c75eabca6b192e162aa149786
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-02-17 03:42:55 -0400

    Implement create (get & post) methods and its views

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -46,13 +46,74 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: item create GET");
+	// Get all categories, which we can use for adding to our item
+	const allCategories = await Category.find().sort({ family_name: 1 }).exec();
+
+	res.render("item_form", {
+		title: "Create Item",
+		categories: allCategories,
+	});
 });
 
 // Handle item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: item create POST");
-});
+exports.item_create_post = [
+	// Validate and sanitize fields.
+	body("name", "name must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("description", "description must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("price", "price must not be empty")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("number_in_stock", "number in stock must not be empty")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("category", "Category must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a Book object with escaped and trimmed data.
+		const item = new Item({
+			name: req.body.name,
+			description: req.body.description,
+			price: req.body.price,
+			number_in_stock: req.body.number_in_stock,
+			category: req.body.category,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all authors and genres for form.
+			const allCategories = await Category.find()
+				.sort({ name: 1 })
+				.exec();
+
+			res.render("book_form", {
+				title: "Create Item",
+				categories: allCategories,
+				item: item,
+				errors: errors.array(),
+			});
+		} else {
+			// Data from form is valid. Save item.
+			await item.save();
+			res.redirect(item.url);
+		}
+	}),
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
